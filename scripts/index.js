753f8902a4ebe489732b58691c2c0ee6c1b8288c
unknown <kshitijnairr@gmail.com>
2022-10-16 18:18:10 -0700

    added localStorage for theme setting

--- scripts/index.js
+++ scripts/index.js
@@ -11,17 +11,21 @@ if (userTheme === 'light') {
     toggleTheme();
 }
 
-
 function toggleTheme() {
     themeBtn.classList.toggle("light");
     themeBtn.classList.toggle("dark");
     let themeClass = themeBtn.classList;
+    let bodyElement = document.body;
     // change theme here
     if (themeClass.contains("light")) {
-        themeBtn.innerHTML = "Dark Mode"
+        themeBtn.innerHTML = "Dark Mode";
+        bodyElement.style.backgroundImage = 'linear-gradient( rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) ), url("./assets/light-index.jpg")';
+        bodyElement.style.color = "rgb(20,20,20)";
         localStorage.setItem('userTheme', 'light');
     } else {
-        themeBtn.innerHTML = "Light Mode"
+        themeBtn.innerHTML = "Light Mode";
+        bodyElement.style.backgroundImage = 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/light-index.jpg")';
+        bodyElement.style.color = "rgb(220, 220, 220)";
         localStorage.setItem('userTheme', 'dark');
     }
     console.log(localStorage.getItem('userTheme'))
