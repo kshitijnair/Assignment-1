9a6a0555e5718bfe8e0ce490eef5e4d15ad1c8c6
unknown <kshitijnairr@gmail.com>
2022-10-16 22:53:46 -0700

    refactored css and js files into properly named subdirectories

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,3 @@
-// Theme Toggle
-let themeBtn = document.body.querySelector("#themeBtn");
-
-// Set theme 
-let userTheme = localStorage.getItem('userTheme')
-if (userTheme === 'light') {
-    console.log("user wants light theme")
-    // do nothing
-} else if (userTheme === 'dark') {
-    console.log("user wants dark theme")
-    toggleTheme();
-}
-
-function toggleTheme() {
-    themeBtn.classList.toggle("light");
-    themeBtn.classList.toggle("dark");
-    let themeClass = themeBtn.classList;
-    let bodyElement = document.body;
-    // change theme here
-    if (themeClass.contains("light")) {
-        themeBtn.innerHTML = "Dark Mode";
-        bodyElement.classList.add('light-background');
-        bodyElement.classList.remove('dark-background');
-        localStorage.setItem('userTheme', 'light');
-    } else {
-        themeBtn.innerHTML = "Light Mode";
-        bodyElement.classList.add('dark-background');
-        bodyElement.classList.remove('white-background');
-        localStorage.setItem('userTheme', 'dark');
-    }
-    console.log(localStorage.getItem('userTheme'))
-}
-
-themeBtn.addEventListener('click', toggleTheme)
-
 // Date and Time Buttons
 const form = document.body.querySelector("#radioForm")
 form.addEventListener('submit', (e) => {
